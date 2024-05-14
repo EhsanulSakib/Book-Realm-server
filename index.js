bc5d66dc3a253b167459d9fe1509a6a08e86eb80
Kazi Ehsanul Haque <ehsanulsakib.professional@gmail.com>
2024-05-15 01:43:01 +0600

    delete /borrow/:id

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const cors = require('cors');
-require('dotenv').config();
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+  origin:[
+    'http://localhost:5173'
+],
+credentials: true}
+));
 app.use(express.json());
+app.use(cookieParser());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hhwjvgh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -19,6 +27,27 @@ const client = new MongoClient(uri, {
   }
 });
 
+const logger = (req, res, next) =>{
+  console.log('log info: ', req.method, req.url)
+  next()
+}
+
+const verifyToken = (req,res,next)=>{
+  const token = req.cookies?.token
+  // console.log('token in the middleware', token)
+  if(!token){
+      return res.status(401).send({message: 'Unauthorized Access'})
+  }
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decode) =>{
+      if(err){
+          return res.status(401).send({message: 'Unauthorized Access'})
+      }
+      req.user = decode
+      console.log(decode)
+      next()
+  })
+}
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -26,6 +55,28 @@ async function run() {
 
     const booksCollection = client.db("bookRealmDB").collection('books');
     const borrowCollection = client.db("bookRealmDB").collection('Borrow');
+
+    // auth related api
+    app.post('/jwt', logger, async(req,res)=>{
+      const user = req.body
+      console.log('user for token', user);
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET,{
+          expiresIn:'1h'
+      })
+      
+      res.cookie('token', token, {
+          httpOnly: true,
+          secure: true,
+          sameSite: 'none'
+      })
+      .send({success: true})
+  })
+
+
+  app.post('/logout', async(req,res)=>{
+    const user = req.body
+    res.clearCookie('token', {maxAge:0}).send({success:true})
+  })
     
     app.get('/books', async(req,res) =>{
       const cursor = booksCollection.find();
@@ -47,6 +98,19 @@ async function run() {
     const result = await booksCollection.insertOne(newBook);
     res.send(result)
   })
+
+  app.get('/borrow',logger, verifyToken, async (req, res) => {
+    console.log(req.query.email);
+    if(req.user.email !== req.query.email){
+        return res.status(403).send({message: "Forbidden Access"})
+    }
+    let query = {};
+    if (req.query?.email) {
+        query = { userEmail: req.query.email }
+    }
+    const result = await borrowCollection.find(query).toArray();
+    res.send(result);
+})
   
   app.post('/borrow', async(req,res) =>{
     try{      
@@ -71,6 +135,28 @@ async function run() {
     }
     catch(error){
       res.status(500).send({message: error.message}) 
+    }
+  })
+
+  app.delete('/borrow/:id', async(req,res)=>{
+    const {id} = req.params
+    const query = {bookId : id}
+    const result = await borrowCollection.deleteOne(query)
+    console.log({result})
+    if(result.acknowledged && result.deletedCount === 1){
+      const book = await booksCollection.findOne({_id: new ObjectId(id)})
+      await booksCollection.updateOne({_id: new ObjectId(id)}, {
+        $set:{
+          bookQuantity: (parseInt(book.bookQuantity)+1).toString()
+        }
+      })
+      res.status(200).send({success: true, message:"Delete Successfully"})
+    }
+    else if (result.acknowledged && result.deletedCount === 0){
+      res.status(200).send({success: false, message:"No Data Deleted"})
+    }
+    else{
+      res.send(400).send({message: "Invalid Request"})
     }
   })
 
